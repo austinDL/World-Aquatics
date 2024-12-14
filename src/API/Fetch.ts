bcbe449dbc87c69a466165ca23f07f439a985b92
austinDL <austin.r.diggins@gmail.com>
2024-12-14 12:15:14 +1000

    removed testing function call and updated location name for better display

--- src/API/Fetch.ts
+++ src/API/Fetch.ts
@@ -3,7 +3,7 @@ import Event from '../Components/Interfaces'
 
 const BASE_URL: string = 'https://api.worldaquatics.com/fina/events';
 const END_POINTS: Record<string, string> = {
-    budapest: 'b344ceee-7bae-4076-a34a-e019524c72ff'
+    Budapest: 'b344ceee-7bae-4076-a34a-e019524c72ff'
 };
 
 export async function loadEventData(location:string): Promise<Event> {
@@ -11,10 +11,3 @@ export async function loadEventData(location:string): Promise<Event> {
     const response = await axios.get<Event>(url);
     return response.data;
 }
-
-loadEventData('budapest').then(
-    event => {
-        console.log(event.name);
-        console.log(event.heats);
-    }
-)
