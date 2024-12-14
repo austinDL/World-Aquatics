f650cdeac95733b05c2cdb024267feea6deaec20
Austin Diggins <austin.r.diggins@gmail.com>
2024-12-14 17:41:15 +1000

    updated to point to Lambda API

--- src/API/Fetch.ts
+++ src/API/Fetch.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Event, Heat, Result, Split } from '../Components/Interfaces'
 import { EventPayload, HeatPayload, ResultPayload, SplitPayload } from '../Components/PayloadInterfaces';
-const BASE_URL: string = 'https://api.worldaquatics.com/fina/events';
+const BASE_URL: string = 'https://knuptj4lr9.execute-api.ap-southeast-2.amazonaws.com/dev';
 const END_POINTS: Record<string, string> = {
     Budapest: 'b344ceee-7bae-4076-a34a-e019524c72ff'
 };
@@ -10,7 +10,7 @@ export async function loadEventData(location:string): Promise<Event> {
     // Extract the payload of the event in the selected location
     const url: string = `${BASE_URL}/${END_POINTS[location]}`;
     console.log(url);
-    const response = await axios.get<EventPayload>(url);
+    const response = await axios.get<EventPayload>(BASE_URL);
     const data = response.data;
     console.log(response);
 
