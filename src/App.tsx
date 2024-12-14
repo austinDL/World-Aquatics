6ccbe0517a470abfbf274caafc407fe56bf4e355
Austin Diggins <austin.r.diggins@gmail.com>
2024-12-15 08:45:01 +1000

    Add routing to be able to navigate to Heats

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 // import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EventDisplay from './Components/EventDisplay';
 
 function App() {
 
   return (
     <main>
-      <EventDisplay />
+      <Router>
+        <Routes>
+          <Route path="/" element={<EventDisplay />} />
+          <Route path="/heat/:heat_name" element={<div>Heat</div>} />
+        </Routes>
+      </Router>
     </main>
   );
 }
