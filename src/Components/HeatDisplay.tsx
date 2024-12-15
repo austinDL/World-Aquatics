16fc85d56becc94dfc67fc7596c598c3d2ba8559
Austin Diggins <austin.r.diggins@gmail.com>
2024-12-15 16:26:33 +1000

    add pop-up for splits

--- src/Components/HeatDisplay.tsx
+++ src/Components/HeatDisplay.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
-import { Heat, Swimmer } from './Interfaces';
+import { Heat, Swimmer, Split } from './Interfaces';
+import { ReturnHome } from './Buttons';
+import SplitDisplay from './SplitDisplay';
 import './Components.css';
 
 const HeatDisplay: React.FC = () => {
     const { state } = useLocation();
     const heat: Heat = state.heat;
+    // state of splits popups
+    const [active_splits, set_active_splits] = useState<Split[]>([]);
+    const [active_swimmer, set_active_swimmer] = useState<Swimmer|null>(null);
+    const [is_popup, set_is_popup] = useState<boolean>(false);
 
     const navigate = useNavigate();
     function handle_swimmer_click(swimmer:Swimmer) {
@@ -14,16 +20,26 @@ const HeatDisplay: React.FC = () => {
             state: { swimmer }
         });
     }
+    function handle_split_click(splits:Split[], swimmer:Swimmer) {
+        set_active_splits(splits.sort((a,b) => a.order - b.order));
+        set_active_swimmer(swimmer);
+        set_is_popup(true);
+    }
+    function deactivate() {
+        set_is_popup(false);
+    }
 
     // Define the Heat properties to display
     const rows: GridRowsProp = heat.results.map(result => ({
         id: result.id,
         athlete_name: `${result.swimmer.first_name} ${result.swimmer.last_name}`,
         country: result.swimmer.NAT,
-        reaction_time: result.reaction_time,
-        time: result.time,
-        time_behind: result.time_behind || "Winner", // If time behind is null, then they are the winner of the heat
-        swimmer: result.swimmer
+        reaction_time: result.reaction_time || 'N/A',
+        time: result.time || 'N/A',
+        time_behind: result.time_behind ? result.time_behind : result.time ? "Winner" : "N/A", // If time behind is null, then they are the winner of the heat
+        swimmer: result.swimmer,
+        splits: result.splits,
+        view_splits: 'View Splits'
     }))
 
     // Define the grid structure
@@ -52,13 +68,28 @@ const HeatDisplay: React.FC = () => {
         {
             field: "time_behind",
             headerName: "Time Behind (s)"
+        },
+        {
+            field: "view_splits",
+            headerName: "",
+            renderCell: (params) => (
+                <span className='clickable inline-button' onClick={() => handle_split_click(params.row.splits, params.row.swimmer)}>
+                    {params.value}
+                </span>
+            )
         }
     ]
 
     return (
         <div>
+            <ReturnHome />
             <h1>{heat.name}</h1>
-            <DataGrid rows={rows} columns={columns} />
+            <DataGrid 
+                pagination
+                rows={rows} 
+                columns={columns}
+            />
+            <SplitDisplay splits={active_splits} swimmer={active_swimmer} is_active={is_popup} on_close={deactivate}/>
         </div>
     );
 }
---
+++ src/Components/SplitDisplay.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button } from '@mui/material';
+import { Split, Swimmer } from './Interfaces';
+
+interface SplitParams {
+    splits: Split[] | null,
+    swimmer: Swimmer | null,
+    is_active: boolean,
+    on_close: () => void
+}
+
+const SplitDisplay: React.FC<SplitParams> = ({ splits, swimmer, is_active, on_close }) => {
+    const swimmer_name = `${swimmer?.first_name} ${swimmer?.last_name}`;
+    const is_splits = splits !== null && splits.length > 0;
+    const title: string = is_splits ? `${swimmer_name} Splits` : `No Splits Available for: ${swimmer_name}`;
+    return (
+        <Dialog open={is_active} onClose={() => on_close()}>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogContent>
+                <div>
+                    {is_splits ? <h3>Splits</h3> : <span></span>}
+                    <ul>
+                        {splits?.map(split => (
+                            <li key={split.order}>
+                                Split {split.order}: {split.time} s
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={() => on_close()} color="primary">
+                    Close
+                </Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+export default SplitDisplay;
