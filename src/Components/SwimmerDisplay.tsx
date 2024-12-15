257aee16587eaa8fac6dedf73eb4265b1483bfbe
Austin Diggins <austin.r.diggins@gmail.com>
2024-12-15 13:40:03 +1000

    add more details to swimmer - need to fetch photo

--- src/Components/SwimmerDisplay.tsx
+++ src/Components/SwimmerDisplay.tsx
@@ -5,9 +5,13 @@ import { Swimmer } from "./Interfaces";
 const SwimmerDisplay: React.FC = () => {
     const { state } = useLocation();
     const swimmer: Swimmer = state.swimmer;
+    const full_name: string = `${swimmer.first_name} ${swimmer.last_name}`;
 
     return (
-        <div>{swimmer.first_name} {swimmer.last_name}</div>
+        <div>
+            <h1>{full_name}</h1>
+            <p>At {swimmer.age} years old, {full_name} has been competing as a swimmer for their home country of {swimmer.NAT}</p>
+        </div>
     )
 }
 
