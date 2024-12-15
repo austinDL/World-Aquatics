f8ccef43df11a3c3ebc918107d26f034fd6188fc
Austin Diggins <austin.r.diggins@gmail.com>
2024-12-15 16:26:53 +1000

    add button for returning to home page

--- src/Components/SwimmerDisplay.tsx
+++ src/Components/SwimmerDisplay.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { Swimmer } from "./Interfaces";
+import { ReturnHome } from './Buttons';
 
 const SwimmerDisplay: React.FC = () => {
     const { state } = useLocation();
     const swimmer: Swimmer = state.swimmer;
-    const full_name: string = `${swimmer.first_name} ${swimmer.last_name}`;
 
+    const full_name: string = `${swimmer.first_name} ${swimmer.last_name}`;
     return (
         <div>
+            <ReturnHome />
             <h1>{full_name}</h1>
             <p>At {swimmer.age} years old, {full_name} has been competing as a swimmer for their home country of {swimmer.NAT}</p>
         </div>
